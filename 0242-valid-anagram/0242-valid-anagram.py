fb2a76a679baae7f3a779f90fc904bb648002207
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-16 14:12:52 +0530

    Time: 11 ms (73.28%), Space: 17.8 MB (62.72%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        dictHash  = {}
+        for i in s:
+            if i in dictHash:
+                dictHash[i]=dictHash[i]+1
+            else:
+                dictHash[i]=1
+
+        for i in  t:
+            if i in dictHash and dictHash[i]>0:
+                dictHash[i]=dictHash[i]-1
+            else:
+                return False
+
+        return True
+
+        
