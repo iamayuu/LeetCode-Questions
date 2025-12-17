f80fc6c7f2a63710032e302dc0bc65d366e2a332
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-17 20:05:59 +0530

    Time: 22 ms (66.98%), Space: 23.3 MB (55.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,15 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix_multi = [1]*len(nums)
-        suffix_multi = [1]*len(nums)
-        result = []
+        #Solution 1
+        # prefix_multi = [1]*len(nums)
+        # suffix_multi = [1]*len(nums)
+        # result = []
         
-        for i in range(len(nums)):
-            if i>0:
-                prefix_multi[i]=prefix_multi[i-1] * nums[i-1]
-                suffix_multi[len(nums)-1-i]=suffix_multi[len(nums)-i]*nums[len(nums)-i]
+        # for i in range(len(nums)):
+        #     if i>0:
+        #         prefix_multi[i]=prefix_multi[i-1] * nums[i-1]
+        #         suffix_multi[len(nums)-1-i]=suffix_multi[len(nums)-i]*nums[len(nums)-i]
         
-        for i in range(len(nums)):
-            result.append(prefix_multi[i]*suffix_multi[i])
+        # for i in range(len(nums)):
+        #     result.append(prefix_multi[i]*suffix_multi[i])
 
+        # return result
+
+        #Solution 2
+        result = [1]*len(nums)
+        suffix_multi = 1
+        for i in range(1,len(nums)):
+            result[i]=result[i-1] * nums[i-1]
+
+        for i in range(len(nums)-1,-1,-1):
+            result[i]=result[i]*suffix_multi
+            suffix_multi=suffix_multi*nums[i]
+                
         return result
