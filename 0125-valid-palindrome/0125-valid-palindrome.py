9406015ebc9de34ca38ef538f557974c4ad1c05f
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-18 18:12:34 +0530

    Time: 3 ms (99.02%), Space: 19.1 MB (26.9%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,14 +4,5 @@ class Solution:
         s = re.sub(r'[^a-zA-Z0-9]', '', s)
         s = str.lower(s)
 
-        start = 0
-        end = len(s)-1
-
-        for i in range(int(len(s)/2)):
-            if s[start]==s[end]:
-                start+=1
-                end-=1
-            else:
-                return False
-        return True
+        return s==s[::-1]
 
