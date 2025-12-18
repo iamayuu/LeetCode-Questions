65647d373304178a06b3092c9325375c8943e98f
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-18 12:23:38 +0530

    Time: 55 ms (42.09%), Space: 34.4 MB (17.7%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,24 +1,40 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        #Solution 1 (Time = O(n log n))
+        # if nums == []:
+        #     return 0
+        # nums.sort()
+        # max_length = 1
+        # current_length = 1
+        # n = nums[0]
+        # for i in range(1, len(nums)):
+        #     if nums[i]==n+1:
+        #         n = nums[i]
+        #         current_length +=1
+        #     elif nums[i]==n:
+        #         n = nums[i]
+        #     else:
+        #         current_length=1
+        #         n = nums[i]
+        #     if current_length >= max_length:
+        #         max_length=current_length
+
+        # return max_length
+
+        #Solution 2 (Time = O(n))
         if nums == []:
-            return 0
-        nums.sort()
+            return 0        
+        my_set = set(nums)
         max_length = 1
-        current_length = 1
-        n = nums[0]
-        for i in range(1, len(nums)):
-            if nums[i]==n+1:
-                n = nums[i]
-                current_length +=1
-            elif nums[i]==n:
-                n = nums[i]
-            else:
-                current_length=1
-                n = nums[i]
-            if current_length >= max_length:
-                max_length=current_length
-
-
+        for num in my_set: 
+            if num-1 not in my_set:
+                for i in range(1,len(my_set)):
+                    if num+i in my_set:
+                        if i+1>=max_length:
+                            max_length =i+1
+                    else:
+                        break
+        
         return max_length
 
 
