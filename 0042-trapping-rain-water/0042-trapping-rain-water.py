60551ed0fd6f602aa895a61ed38704cd65c55269
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-19 15:54:14 +0530

    Time: 31 ms (19.34%), Space: 19.3 MB (68.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -7,14 +7,19 @@ class Solution:
         left_max = [0]*len_height
         right_max = [0]*len_height
 
-        left_max[0]=height[0]
-        right_max[len_height-1]=height[len_height-1]
-
-        for i in range(len_height-1):
-            left_max[i]=max(left_max[i-1],height[i])
-
-        for i in range(len_height-2,0,-1):
-            right_max[i]=max(right_max[i+1],height[i])
+        for i in range(len_height):
+            if i==0:
+                left_max[i]=height[i]
+                right_max[len_height-1-i]=height[len_height-1-i]
+            else:
+                if left_max[i-1]<height[i]:
+                    left_max[i]=height[i]
+                else:
+                    left_max[i]=left_max[i-1]
+                if right_max[len_height-1-i+1]<height[len_height-1-i]:
+                    right_max[len_height-1-i]=height[len_height-1-i]
+                else:
+                    right_max[len_height-1-i]=right_max[len_height-1-i+1]
 
         result = 0
         for i in range(1,len_height-1):
