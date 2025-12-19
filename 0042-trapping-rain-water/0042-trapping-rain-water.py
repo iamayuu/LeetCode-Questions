f229f0de94856165143d99c80f205924495e972d
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-19 16:07:32 +0530

    Time: 32 ms (14.46%), Space: 19 MB (99.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,30 +1,51 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         #Solution 1 - Array pre processing
-        len_height = len(height)
-        if len_height<3:
+        # len_height = len(height)
+        # if len_height<3:
+        #     return 0
+        # left_max = [0]*len_height
+        # right_max = [0]*len_height
+
+        # for i in range(len_height):
+        #     if i==0:
+        #         left_max[i]=height[i]
+        #         right_max[len_height-1-i]=height[len_height-1-i]
+        #     else:
+        #         if left_max[i-1]<height[i]:
+        #             left_max[i]=height[i]
+        #         else:
+        #             left_max[i]=left_max[i-1]
+        #         if right_max[len_height-1-i+1]<height[len_height-1-i]:
+        #             right_max[len_height-1-i]=height[len_height-1-i]
+        #         else:
+        #             right_max[len_height-1-i]=right_max[len_height-1-i+1]
+
+        # result = 0
+        # for i in range(1,len_height-1):
+        #     water_at_i = min(left_max[i],right_max[i])-height[i]
+        #     water_at_i = max(0, water_at_i) #if somehow -ve water comes up
+        #     result = result+water_at_i
+
+        # return result
+
+        #Solution 2 - 2 Pointer Approach
+        if len(height)<3:
             return 0
-        left_max = [0]*len_height
-        right_max = [0]*len_height
-
-        for i in range(len_height):
-            if i==0:
-                left_max[i]=height[i]
-                right_max[len_height-1-i]=height[len_height-1-i]
-            else:
-                if left_max[i-1]<height[i]:
-                    left_max[i]=height[i]
-                else:
-                    left_max[i]=left_max[i-1]
-                if right_max[len_height-1-i+1]<height[len_height-1-i]:
-                    right_max[len_height-1-i]=height[len_height-1-i]
-                else:
-                    right_max[len_height-1-i]=right_max[len_height-1-i+1]
-
+        left = 0
+        right = len(height)-1
+        left_max = 0
+        right_max = 0
         result = 0
-        for i in range(1,len_height-1):
-            water_at_i = min(left_max[i],right_max[i])-height[i]
-            water_at_i = max(0, water_at_i) #if somehow -ve water comes up
-            result = result+water_at_i
+        while left<right:
+            left_max = max(left_max, height[left])
+            right_max = max(right_max, height[right])
 
+            if left_max<right_max:
+                result = result+max(0, left_max-height[left])
+                left+=1
+            else:
+                result = result+max(0, right_max-height[right])
+                right-=1
         return result
+
