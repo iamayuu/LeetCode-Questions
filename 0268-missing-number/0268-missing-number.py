2f96c4ee87323b74c04306d4ad90aade4c5df444
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-26 20:33:19 +0530

    Time: 5 ms (40.79%), Space: 18.5 MB (91.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,26 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         #Solution 1 (using sum formula)
-        n = len(nums)
-        total_sum = (n*(n+1))/2
-        my_sum = 0
-        for n in nums:
-            my_sum +=n
+        # n = len(nums)
+        # total_sum = (n*(n+1))/2
+        # my_sum = 0
+        # for n in nums:
+        #     my_sum +=n
 
-        return int(total_sum-my_sum)
+        # return int(total_sum-my_sum)
+
+        #Solution 2 (Using XOR operation)
+        # ans = 0
+        # for n in nums:
+        #     ans = ans^n
+        # for n in range(len(nums)+1):
+        #     ans = ans^n
+        # return ans
+
+        #Solutio 3 (Using sorting and indexs to match)
+        nums.sort()
+        for i in range(len(nums)):
+            if i!=nums[i]:
+                return i
+        return len(nums)
+
