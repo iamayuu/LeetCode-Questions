3bea6cb1e19e0bbb86dde3414abe48ef6a4c2440
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-30 22:46:49 +0530

    Time: 0 ms (100%), Space: 17.1 MB (98.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,12 +3,21 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        #Solution 1 (Usin extra space O(n^2))
+        #Solution 1 (Time - O(n^2) and Using extra space O(n^2))
+        # n=len(matrix)
+        # result = [[0 for x in range(n)] for y in range(n)]
+        # for i in range(n):
+        #     for j in range(n):
+        #         result[j][n-1-i]=matrix[i][j]
+        # matrix[:]=result
+
+        #Solution 2 (Transpose Matrix) (Without using extra space) (Time - 0(n^2) Space - O(1))
         n=len(matrix)
-        result = [[0 for x in range(n)] for y in range(n)]
         for i in range(n):
-            for j in range(n):
-                result[j][n-1-i]=matrix[i][j]
-        matrix[:]=result
+            for j in range(i,n):
+                matrix[i][j], matrix[j][i]=matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i]=matrix[i][::-1]
+            
 
         
