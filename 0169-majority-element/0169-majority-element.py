a4c021d862d2d041b6bcec64c3a4c9d1ef29f78e
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-30 11:37:48 +0530

    Time: 1 ms (87.17%), Space: 19.2 MB (94.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -6,7 +6,19 @@ class Solution:
         # return count.most_common(1)[0][0]
 
         #Solution 2 (using counter and finding value with >n/2)
-        count = Counter(nums)
-        for i in count:
-            if count[i]>len(nums)/2:
-                return i
+        # count = Counter(nums)
+        # for i in count:
+        #     if count[i]>len(nums)/2:
+        #         return i
+
+        #Solution 3 (Using majority candidate)
+        candidate = 0
+        votes = 0
+        for n in nums:
+            if votes ==0:
+                candidate=n        
+            if candidate==n:
+                votes+=1
+            else:
+                votes-=1
+        return candidate
