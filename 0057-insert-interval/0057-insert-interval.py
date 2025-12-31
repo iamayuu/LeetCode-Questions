5b720352632a6b3b8241159f2fedcc605db349fd
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2025-12-31 20:50:25 +0530

    Time: 1 ms (63.9%), Space: 19.1 MB (90.51%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,34 +1,54 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        start = 0
-        end = len(intervals)-1
-        if len(intervals)==0:
-            intervals.append(newInterval)
-        elif intervals[0][0]>newInterval[0]:
-            intervals.insert(start, newInterval)
-        elif intervals[-1][0]<newInterval[0]:
-            intervals.append(newInterval)
-        else :
-            while start<=end:
-                mid = (start+end)//2
-                if start==mid:
-                    if intervals[start][0]<=newInterval[0]:
-                        intervals.insert(start+1, newInterval)
-                    else:
-                        intervals.insert(start, newInterval)
-                    break
-                elif end==mid:
-                    intervals.insert(end, newInterval)
-                    break
-                elif intervals[start][0]<=newInterval[0]<=intervals[mid][0]:
-                    end=mid
-                else:
-                    start=mid
+        #Solution1 (Brute Force)
+        # if len(intervals)==0:
+        #     intervals.append(newInterval)
+        # elif intervals[0][0]>newInterval[0]:
+        #     intervals.insert(start, newInterval)
+        # elif intervals[-1][0]<newInterval[0]:
+        #     intervals.append(newInterval)
+        # else :
+        #     start = 0
+        #     end = len(intervals)-1
+        #     while start<=end:
+        #         mid = (start+end)//2
+        #         if start==mid:
+        #             if intervals[start][0]<=newInterval[0]:
+        #                 intervals.insert(start+1, newInterval)
+        #             else:
+        #                 intervals.insert(start, newInterval)
+        #             break
+        #         elif end==mid:
+        #             intervals.insert(end, newInterval)
+        #             break
+        #         elif intervals[start][0]<=newInterval[0]<=intervals[mid][0]:
+        #             end=mid
+        #         else:
+        #             start=mid
+        # result = []
+        # result.append(intervals[0])
+        # for i in range(1, len(intervals)):
+        #     if result[-1][1]>=intervals[i][0]:
+        #         result[-1][1]=max(intervals[i][1],result[-1][1])
+        #     else:
+        #         result.append(intervals[i])
+        # return result
+    
+        #Solution2 (Using Greedy Algorithm)
         result = []
-        result.append(intervals[0])
-        for i in range(1, len(intervals)):
-            if result[-1][1]>=intervals[i][0]:
-                result[-1][1]=max(intervals[i][1],result[-1][1])
-            else:
-                result.append(intervals[i])
+        i = 0
+        n = len(intervals)
+        merge_low = newInterval[0]
+        merge_high = newInterval[1]
+        while i<n and intervals[i][1]<newInterval[0]:
+            result.append(intervals[i])
+            i+=1
+        while i<n and intervals[i][0]<=newInterval[1]:
+            merge_low = min(intervals[i][0], merge_low)
+            merge_high = max(intervals[i][1], merge_high)
+            i+=1
+        result.append([merge_low, merge_high])
+        while i<n:
+            result.append(intervals[i])
+            i+=1
         return result
