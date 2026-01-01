dd3bed6ec0e6adce72fa77c869f182aaf9ad38ce
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-01-01 20:13:56 +0530

    Time: 30 ms (62.15%), Space: 20.3 MB (86.08%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,35 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        #Solution1 (Brute Force) [Time - O(n^2)]
+        # total_subarray = 0
+        # for i in range(len(nums)):
+        #     sum=0
+        #     for j in range(i, len(nums)):
+        #         sum+=nums[j]
+        #         if sum==k:
+        #             total_subarray+=1
+        # return total_subarray
+
+        #Solution2 (Optimal)
+        total_subarray = 0
+
+        #Array Pre-Processing
+        prefix_sum = []
+        curr_sum=0
+        for n in nums:
+            curr_sum+=n
+            prefix_sum.append(curr_sum)
+        
+        counter = {}
+        #Finding subarray with sum k and calculating counters
+        for curr_psum in prefix_sum:
+            if curr_psum-k==0:
+                total_subarray+=1
+            if curr_psum-k in counter:
+                total_subarray+=counter[curr_psum-k]
+            if curr_psum in counter:
+                counter[curr_psum]+=1
+            else:
+                counter[curr_psum]=1
+
+        return total_subarray
