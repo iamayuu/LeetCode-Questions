b9dc10d49b798f6c0477b739ab96d2a9a1f14627
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-05 12:24:40 +0530

    Time: 64 ms (30.49%), Space: 19.3 MB (34.53%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -1,10 +1,18 @@
 class Solution:
     def constructTransformedArray(self, nums: List[int]) -> List[int]:
+        #Solution1
+        # result = []
+        # n = len(nums)
+        # for i in range(n):
+        #     if nums[i]==0:
+        #         result.append(nums[i])
+        #     else:
+        #         result.append(nums[(i+nums[i])%n])
+        # return result
+
+        #Solution2
         result = []
         n = len(nums)
         for i in range(n):
-            if nums[i]==0:
-                result.append(nums[i])
-            else:
-                result.append(nums[(i+nums[i])%n])
+            result.append(nums[(i+nums[i])%n])
         return result
