13dd26d00ef68fee411d05594d596a00f025758a
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-08 00:09:06 +0530

    Time: 189 ms (73.97%), Space: 20.6 MB (33.42%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,23 +1,38 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        #Solution1 (Brute Force)
+        #Solution1 (Array Preprocessing)
         n = len(s)
         #Array preprocessing - count how many a to right of an index and how many B to left of index
-        a_right = [0]*n
-        a_count = 0
-        for i in range(n-1, -1, -1):
-            a_right[i] = a_count
-            if s[i] == 'a':
-                a_count+=1
+        # a_right = [0]*n
+        # a_count = 0
+        # for i in range(n-1, -1, -1):
+        #     a_right[i] = a_count
+        #     if s[i] == 'a':
+        #         a_count+=1
 
-        b_left = [0]*n
-        b_count = 0
-        for i in range(n):
-            b_left[i] = b_count
-            if s[i] == 'b':
-                b_count+=1
+        # b_left = [0]*n
+        # b_count = 0
+        # for i in range(n):
+        #     b_left[i] = b_count
+        #     if s[i] == 'b':
+        #         b_count+=1
 
-        ans = float("inf")
+        # ans = float("inf")
+        # for i in range(n):
+        #     ans = min(ans, a_right[i]+ b_left[i])
+        # return ans
+
+        #Solution2 (Using Stack -LIFO)
+        stack = []
+        count = 0
+        n = len(s)
         for i in range(n):
-            ans = min(ans, a_right[i]+ b_left[i])
-        return ans
+            if s[i]=='a':
+                if stack and stack[-1]=='b':
+                    stack.pop()
+                    count+=1
+                else:
+                    stack.append('a')
+            else:
+                stack.append('b')
+        return count
