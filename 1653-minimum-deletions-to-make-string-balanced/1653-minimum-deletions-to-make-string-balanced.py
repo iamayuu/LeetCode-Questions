84b2fa8316a9a7f0f1ad08fc10b18dbdc8ad64d8
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-08 00:25:02 +0530

    Time: 406 ms (41.92%), Space: 23 MB (21.64%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -22,17 +22,36 @@ class Solution:
         #     ans = min(ans, a_right[i]+ b_left[i])
         # return ans
 
-        #Solution2 (Using Stack -LIFO)
-        stack = []
-        count = 0
+        #Solution2 (Using Stack - LIFO)
+        # stack = []
+        # count = 0
+        # n = len(s)
+        # for i in range(n):
+        #     if s[i]=='a':
+        #         if stack and stack[-1]=='b':
+        #             stack.pop()
+        #             count+=1
+        #         else:
+        #             stack.append('a')
+        #     else:
+        #         stack.append('b')
+        # return count
+
+        #Solution3 (Optimised Solution 1)
         n = len(s)
+        #Array preprocessing - count how many a to right of an index and how many B to left of index
+        a_right = [0]*n
+        a_count = 0
+        for i in range(n-1, -1, -1):
+            a_right[i] = a_count
+            if s[i] == 'a':
+                a_count+=1
+        
+        ans = float("inf")
+        b_count=0
         for i in range(n):
-            if s[i]=='a':
-                if stack and stack[-1]=='b':
-                    stack.pop()
-                    count+=1
-                else:
-                    stack.append('a')
-            else:
-                stack.append('b')
-        return count
+            ans = min(ans,a_right[i]+b_count)
+            if s[i]=='b':
+                b_count+=1
+        
+        return ans
