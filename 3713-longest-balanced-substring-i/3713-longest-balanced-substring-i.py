df861e10b67d85d4db28708eb6e0c87685e9aaee
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-12 09:24:22 +0530

    Time: 3332 ms (29.56%), Space: 19.3 MB (62.89%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestBalanced(self, s: str) -> int:
+        #Solution1 (Brute Force) [O(n^3) - TLE]
+        # ans = 0
+        # n= len(s)
+        # for i in range(n):
+        #     for j in range(i,n):
+        #         subs = s[i:j+1]
+        #         counter = Counter(subs)
+        #         if len(set(counter.values()))==1:
+        #             ans=max(ans, len(subs))
+        # return ans
+
+        #Solution 2
+        ans = 0
+        n= len(s)
+        for i in range(n):
+            counter = [0]*27
+            for j in range(i,n):
+                counter[ord(s[j])-97]+=1
+                if len(set(counter))==2:
+                    ans=max(ans, j-i+1)
+        return ans                
