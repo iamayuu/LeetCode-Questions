69ec873cb7a60a3d9eb972af950a97138c09e4fd
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-13 19:48:04 +0530

    Time: 883 ms (81.75%), Space: 45.5 MB (59.12%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.py
@@ -1,3 +1,30 @@
+def case2helperfunc(s: str, ch1, ch2):
+    hmap = dict()
+    count_ch1 = 0
+    count_ch2 = 0
+    maxlen = 0
+    for i,c in enumerate(s):
+        if c != ch1 and c !=ch2:
+            hmap.clear()
+            count_ch1=0
+            count_ch2=0
+            continue
+
+        if c==ch1:
+            count_ch1+=1
+        if c==ch2:
+            count_ch2+=1
+        
+        if count_ch1==count_ch2:
+            maxlen = max(maxlen, count_ch1+count_ch2)
+        
+        diff = count_ch1-count_ch2
+        if diff in hmap:
+            maxlen = max(maxlen, i-hmap[diff])
+        else:
+            hmap[diff]=i
+    return maxlen
+
 class Solution:
     def case2helperfunc(self, s: str, ch1, ch2):
         hmap = dict()
@@ -41,9 +68,9 @@ class Solution:
             ans = max(ans,crr_count)
 
         #Case2 - two distinct chars are making ans
-        ans = max(ans, self.case2helperfunc(s, 'a' , 'b'))
-        ans = max(ans, self.case2helperfunc(s, 'b' , 'c'))
-        ans = max(ans, self.case2helperfunc(s, 'a' , 'c'))
+        ans = max(ans, case2helperfunc(s, 'a' , 'b'))
+        ans = max(ans, case2helperfunc(s, 'b' , 'c'))
+        ans = max(ans, case2helperfunc(s, 'a' , 'c'))
 
         #Case3  - all three chars are used in making the longest substring
         countA = 0
