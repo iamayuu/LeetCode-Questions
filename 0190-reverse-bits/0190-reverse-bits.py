ffcaf7183fe7059cd687400aa605c5916c86eb15
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-02-16 14:37:23 +0530

    Time: 55 ms (5.77%), Space: 19.4 MB (22.24%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,19 +1,26 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         #Brute Force
-        binary = ""
-        while n>0:
-            bit = n%2
-            n=n//2
-            binary = str(bit)+binary
-        binary = binary.zfill(32)
+        # binary = ""
+        # while n>0:
+        #     bit = n%2
+        #     n=n//2
+        #     binary = str(bit)+binary
+        # binary = binary.zfill(32)
 
-        offset = 1
-        res = 0
-        for bit in binary:
-            res+= int(bit)*offset
-            offset*=2
-        return res
+        # offset = 1
+        # res = 0
+        # for bit in binary:
+        #     res+= int(bit)*offset
+        #     offset*=2
+        # return res
 
         #Optimized
-        
+        res = 0
+        for i in range(32):
+            check_bit = n&(1<<i)
+            if check_bit:
+                res = res|(1<<(31-i))
+            else:
+                res=res&~(1<<(31-i))
+        return res
