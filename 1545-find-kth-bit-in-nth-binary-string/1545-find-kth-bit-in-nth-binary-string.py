d505b124ebaa2cc7f1f64a4449fde5c9443e9e3c
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-03-03 16:35:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (72.03%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,20 +1,41 @@
-def cal_S(n):
-    if n==1:
-        return "0"
-    else:
-        S_n_1 = cal_S(n-1)
-        invert_S_n_1 = ""
-        for c in S_n_1:
-            if c=="0":
-                invert_S_n_1+="1"
-            else:
-                invert_S_n_1+="0"
+# #Solution 1 (Brute Force)
+# def cal_S(n):
+#     if n==1:
+#         return "0"
+#     else:
+#         S_n_1 = cal_S(n-1)
+#         invert_S_n_1 = ""
+#         for c in S_n_1:
+#             if c=="0":
+#                 invert_S_n_1+="1"
+#             else:
+#                 invert_S_n_1+="0"
         
-        S = S_n_1+"1"+invert_S_n_1[::-1]
-        return S
+#         S = S_n_1+"1"+invert_S_n_1[::-1]
+#         return S
 
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        #Solution 1 (Brute Force)
-        S = cal_S(n)
-        return S[k-1]
+        # #Solution 1 (Brute Force)
+        # S = cal_S(n)
+        # return S[k-1]
+
+        #Solution 2 (Optimized)
+        if n==1:
+            return "0"
+        
+        len=(2**n)-1
+        mid = (len+1)/2
+
+        if k==mid:
+            #k in mid
+            return "1"
+        elif k<mid:
+            #k in left of mid
+            return self.findKthBit(n-1,k)
+        else:
+            #k in right of mid
+            k = (2**n) - k
+            bit = self.findKthBit(n-1,k)
+            return "0" if bit=="1" else "1"
+
