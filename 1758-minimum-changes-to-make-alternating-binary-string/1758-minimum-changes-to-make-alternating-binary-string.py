f9fe0591fe3555dee48df0a8d872c694e241d83d
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-03-05 15:18:57 +0530

    Time: 7 ms (83.47%), Space: 19.3 MB (56.18%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -5,18 +5,17 @@ class Solution:
             return 0
         
         count1 = 0
-        req = "1"
-        for bit in s:
-            if bit!=req:
-                count1 += 1
-            req="0" if req=="1" else "1"
-        
+        req1 = "1"
         count2 = 0
-        req = "0"
+        req2 = "0"
+
         for bit in s:
-            if bit!=req:
+            if bit!=req1:
+                count1 += 1
+            if bit!=req2:
                 count2 += 1
-            req="0" if req=="1" else "1"
+            req1="0" if req1=="1" else "1"
+            req2="0" if req2=="1" else "1"
         
         return min(count1,count2)
         
