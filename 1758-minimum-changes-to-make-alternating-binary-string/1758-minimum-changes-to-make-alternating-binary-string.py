ae9ccc5b9445f0b430d3fcd973910b5d1f8fb1f7
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-03-05 15:21:14 +0530

    Time: 3 ms (99.6%), Space: 19.3 MB (56.18%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,21 +1,33 @@
 class Solution:
     def minOperations(self, s: str) -> int:
         #Solution1 (Brute Force)
-        if len(s)==1:
-            return 0
+        # if len(s)==1:
+        #     return 0
         
-        count1 = 0
-        req1 = "1"
-        count2 = 0
-        req2 = "0"
+        # count1 = 0
+        # req1 = "1"
+        # count2 = 0
+        # req2 = "0"
+
+        # for bit in s:
+        #     if bit!=req1:
+        #         count1 += 1
+        #     if bit!=req2:
+        #         count2 += 1
+        #     req1="0" if req1=="1" else "1"
+        #     req2="0" if req2=="1" else "1"
+        
+        # return min(count1,count2)
+
+        #Solution2 (Bit more optimal from soln 1)
+    
+        count = 0
+        req = "1"
 
         for bit in s:
-            if bit!=req1:
-                count1 += 1
-            if bit!=req2:
-                count2 += 1
-            req1="0" if req1=="1" else "1"
-            req2="0" if req2=="1" else "1"
+            if bit!=req:
+                count += 1
+            req="0" if req=="1" else "1"
         
-        return min(count1,count2)
+        return min(count,len(s)-count)
         
