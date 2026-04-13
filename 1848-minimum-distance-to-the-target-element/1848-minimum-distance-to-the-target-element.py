3b98588273889dde10d0eedbd7cc72f2a678039a
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-13 10:10:03 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.72%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.py
@@ -1,12 +1,29 @@
 class Solution:
     def getMinDistance(self, nums: List[int], target: int, start: int) -> int:
         #Solution1 Brute Force
+        # ans = float("inf")
+        # dist = 0
+        # for i in range(len(nums)):
+        #     if nums[i]==target:
+        #         dist = abs(i-start)
+        #         ans = min(ans, dist)
+        #         if ans==0:
+        #             return ans
+        # return ans
+
+        #Soltion2 (2 pointers)
+        left=start
+        right=start
         ans = float("inf")
-        dist = 0
-        for i in range(len(nums)):
-            if nums[i]==target:
-                dist = abs(i-start)
-                ans = min(ans, dist)
-                if ans==0:
-                    return ans
+        while left>=0:
+            if nums[left]==target:
+                ans = min(ans, abs(left-start))
+                break
+            left-=1
+
+        while right<len(nums):
+            if nums[right]==target:
+                ans = min(ans, abs(right-start))
+                break
+            right+=1
         return ans
