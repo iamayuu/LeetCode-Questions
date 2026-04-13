9fb141899feac883241b9d5131a63099e43b514a
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-13 16:27:27 +0530

    Time: 0 ms (100%), Space: 26.5 MB (50.23%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,19 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        #Solution1 (Brute Force - Complexity is O(k*n))
-        # for i in range(k):
-        #     temp = nums[len(nums)-1]
-        #     nums[:]=[temp]+nums[:len(nums)-1]
-
-        #Solution 2 (normalizing the k and slicing it)
-        # if k>len(nums):
-        #     k=k%len(nums)
-        # nums[:]=nums[len(nums)-k:]+nums[:len(nums)-k]
-
-        #Solution3 (Reversing the array)
-        if k>len(nums):
-            k=k%len(nums)        
-        nums.reverse()
-        nums[:]=nums[k-1::-1]+nums[-1:k-1:-1]
-
+        #Solution1 Brute Force
+        n=len(nums)
+        k=k%n
+        nums1 = nums[:n-k]
+        nums2=nums[n-k:]
+        nums[:]=nums2+nums1
