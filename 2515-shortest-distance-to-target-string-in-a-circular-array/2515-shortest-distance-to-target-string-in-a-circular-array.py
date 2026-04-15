e044fa945cb71ec2e904158876257c7dfc6795bd
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-15 13:05:33 +0530

    Time: 50 ms (2.15%), Space: 19.5 MB (4.3%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -4,18 +4,28 @@ class Solution:
             return -1
         if words[startIndex]==target:
             return 0
+        #Solution1 
+        # left = startIndex-1
+        # right = startIndex+1
+        # n=len(words)
+        # ans = float("inf")
+        # while left>-1:
+        #     if words[left]==target:
+        #         ans=min(ans, startIndex-left, abs(n-startIndex+left))
+        #     left-=1
         
-        left = startIndex-1
-        right = startIndex+1
-        n=len(words)
+        # while right<n:
+        #     if words[right]==target:
+        #         ans=min(ans, right-startIndex, abs(n+startIndex-right))
+        #     right+=1
+        # return ans 
+
+        #Solution2
         ans = float("inf")
-        while left>-1:
-            if words[left]==target:
-                ans=min(ans, startIndex-left, abs(n-startIndex+left))
-            left-=1
-        
-        while right<n:
-            if words[right]==target:
-                ans=min(ans, right-startIndex, abs(n+startIndex-right))
-            right+=1
+        n=len(words)
+        for i in range(n):
+            if words[i]==target:
+                print(i)
+                print(startIndex+n-i)
+                ans = min(ans, abs(i-startIndex), abs(startIndex+n-i), abs(n-startIndex+i))
         return ans 
