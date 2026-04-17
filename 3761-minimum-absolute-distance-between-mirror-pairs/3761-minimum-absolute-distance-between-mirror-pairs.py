3558af40691490ec207b958ef324952de5cd2747
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-17 09:50:52 +0530

    Time: 370 ms (15.89%), Space: 42 MB (51.16%) - LeetHub

---
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        #Solution1 Brute Force -  TLE
+        # n = len(nums)
+        # ans = float("inf")
+        # for i in range(n-1):
+        #     num = nums[i]
+        #     rev_s=""
+        #     for s in str(num):
+        #         rev_s = s+rev_s
+        #     rev_num=int(rev_s)
+        #     j=i+1
+        #     while j<n:
+        #         if nums[j]==rev_num:
+        #             ans = min(ans, abs(i-j))
+        #         j+=1
+        # return -1 if ans == float("inf") else ans
+
+        #Solution2 
+        n = len(nums)
+        ans = float("inf")
+        hmap = dict()
+        for i in range(n):
+            num = nums[i]
+            if num in hmap:
+                ans = min(ans, abs(i-hmap[num]))
+
+            rev_s=""
+            for s in str(num):
+                rev_s = s+rev_s
+            rev_num=int(rev_s)
+            hmap[rev_num]=i
+        return -1 if ans == float("inf") else ans
