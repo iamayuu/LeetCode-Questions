ff0c0a33f18eecfc01633d219e258e203f6648c5
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-19 11:54:55 +0530

    Time: 434 ms (13.4%), Space: 36.1 MB (41.58%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        # #Soltuion1 Brute Force
+        # ans = 0
+        # n = len(nums1)
+        # m = len(nums2)
+        # for i in range(n):
+        #     for j in range(i,m):
+        #         if nums1[i] <= nums2[j]:
+        #             ans = max(ans, j-i)
+        #         else:
+        #             break
+        # return ans
+
+        #Solution 2 Using Binary Search for nums2 to find the farthest point
+        ans = 0
+        n = len(nums1)
+        m = len(nums2)
+        for i in range(n):
+            left = i
+            right = m-1
+            j = 0
+            while left<=right:
+                mid = (left+right)//2
+                if nums2[mid]<nums1[i]:
+                    #move left
+                    right=mid-1
+                else:
+                    #move right storing the possible ans
+                    j=mid
+                    left = mid+1   
+            ans = max(ans, j-i)
+        return ans
