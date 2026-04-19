b98ed8fa8c2307b7523339d27a7cb94cb983b098
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-19 13:06:09 +0530

    Time: 36 ms (97.25%), Space: 36.2 MB (21.31%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -13,21 +13,34 @@ class Solution:
         # return ans
 
         #Solution 2 Using Binary Search for nums2 to find the farthest point
+        # ans = 0
+        # n = len(nums1)
+        # m = len(nums2)
+        # for i in range(n):
+        #     left = i
+        #     right = m-1
+        #     j = 0
+        #     while left<=right:
+        #         mid = (left+right)//2
+        #         if nums2[mid]<nums1[i]:
+        #             #move left
+        #             right=mid-1
+        #         else:
+        #             #move right storing the possible ans
+        #             j=mid
+        #             left = mid+1   
+        #     ans = max(ans, j-i)
+        # return ans
+
+        #Solution 3 Using 2 Pointers
         ans = 0
+        i, j = 0, 0
         n = len(nums1)
         m = len(nums2)
-        for i in range(n):
-            left = i
-            right = m-1
-            j = 0
-            while left<=right:
-                mid = (left+right)//2
-                if nums2[mid]<nums1[i]:
-                    #move left
-                    right=mid-1
-                else:
-                    #move right storing the possible ans
-                    j=mid
-                    left = mid+1   
-            ans = max(ans, j-i)
+        while(i<n and j<m):
+            if nums1[i] > nums2[j]:
+                i+=1
+            else:
+                ans = max(ans, j-i)
+                j+=1
         return ans
