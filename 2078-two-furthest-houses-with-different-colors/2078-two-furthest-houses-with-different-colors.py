8e530d60c5398e8028983985147accea2418f404
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-20 10:36:34 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.45%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,10 +1,25 @@
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
         #Solution1 BruteForce
+        # ans = 0
+        # n = len(colors)
+        # for i in range(n-1):
+        #     for j in range(i+1,n):
+        #         if colors[i] != colors[j]:
+        #             ans = max(ans, j-i)
+        # return ans
+
+        #Solution2 
         ans = 0
         n = len(colors)
+        left = colors[0]
+        right = colors[n-1]
+        for i in range(n-1,0,-1):
+            if colors[i] != left:
+                ans = max(ans, i)
+                break
         for i in range(n-1):
-            for j in range(i+1,n):
-                if colors[i] != colors[j]:
-                    ans = max(ans, j-i)
+            if colors[i] != right:
+                ans = max(ans, n-1-i)
+                break
         return ans
