8115073e8416c079b074cdb9f25d7c7d3f7003a1
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-22 11:10:42 +0530

    Time: 8 ms (87.75%), Space: 19.4 MB (78.43%) - LeetHub

--- 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
+++ 2452-words-within-two-edits-of-dictionary/2452-words-within-two-edits-of-dictionary.py
@@ -1,6 +1,28 @@
 class Solution:
     def twoEditWords(self, queries: List[str], dictionary: List[str]) -> List[str]:
         #Solution1 Brute Force
+        # if len(queries[0])<=2:
+        #     return queries
+        # ans = []
+        # for word in queries:
+        #     if word in dictionary:
+        #         ans.append(word)
+        #     else:
+        #         for dic in dictionary:
+        #             matched = 0
+        #             i = 0
+        #             for i in range(len(word)):
+        #                 if word[i]==dic[i]:
+        #                     matched+=1
+        #                 if matched>=(len(word)-2):
+        #                     break
+        #             if matched>=(len(word)-2):
+        #                     ans.append(word)
+        #                     break
+
+        # return ans
+
+        #Solution1 Brute Force
         if len(queries[0])<=2:
             return queries
         ans = []
@@ -9,15 +31,14 @@ class Solution:
                 ans.append(word)
             else:
                 for dic in dictionary:
-                    matched = 0
+                    unmatched = 0
                     i = 0
                     for i in range(len(word)):
-                        if word[i]==dic[i]:
-                            matched+=1
-                        if matched>=(len(word)-2):
+                        if word[i]!=dic[i]:
+                            unmatched+=1
+                        if unmatched==3:
                             break
-                    if matched>=(len(word)-2):
+                    if unmatched<3:
                             ans.append(word)
                             break
-
         return ans
