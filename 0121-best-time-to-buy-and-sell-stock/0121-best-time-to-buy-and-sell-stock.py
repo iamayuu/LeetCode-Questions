79438a3e6374618c1f041cb9deef61562094e98e
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-25 01:25:52 +0530

    Time: 49 ms (56.08%), Space: 28.6 MB (45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # #Solution1 (Brute Force)
-        # profit = 0
-        # max_profit = 0
-        # for i in range(len(prices)-1):
-        #     for j in range(i,len(prices)):
-        #         profit = prices[j]-prices[i]
-        #         if profit>max_profit:
-        #             max_profit = profit
-        # return max_profit
-
-        #Solution 2 (Storing minimum buy price)
-        min_price = prices[0]
+        
+        min_buy_price = float("inf")
         max_profit = 0
-        for p in prices[1:]:
-            if p<min_price:
-                min_price=p
-            elif p-min_price>max_profit:
-                max_profit=p-min_price
+
+        for crr_price in prices:
+            min_buy_price = min(min_buy_price, crr_price)
+            max_profit = max(max_profit, crr_price-min_buy_price)
+
         return max_profit
