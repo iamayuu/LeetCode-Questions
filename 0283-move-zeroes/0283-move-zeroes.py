51a85fa3c7b78f848c57e1f1d121f744afb48ea0
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-25 19:54:51 +0530

    Time: 3 ms (82.64%), Space: 20.5 MB (26.95%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        a=0
-        for i in range(len(nums)):
-            if nums[i]!=0:
-                temp=nums[i]
-                nums[i]=nums[a]
-                nums[a]=temp
-                a+=1
-                
+        pointer = 0
+        for i,num in enumerate(nums):
+            if num!=0:
+                nums[pointer], nums[i] = nums[i], nums[pointer]
+                pointer+=1
