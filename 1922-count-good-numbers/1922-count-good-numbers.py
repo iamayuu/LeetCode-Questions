3ce93c79f246448e4157b3c6dd9edaf036439fbd
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-26 18:57:02 +0530

    Time: 3 ms (21.78%), Space: 19.1 MB (91.99%) - LeetHub

---
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod = (10**9 +7)
+        if n&1==0:
+            odd, even = n//2, n//2
+            return (pow(5,even,mod)*pow(4,odd,mod))%mod
+        else:
+            even = (n+1)//2
+            odd = n//2
+            return (pow(5,even,mod)*pow(4,odd,mod))%mod
