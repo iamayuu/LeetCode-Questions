36b60a9991a67a7ba3c78694c92f1d66e8ea8056
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-29 19:28:51 +0530

    Time: 11 ms (46.37%), Space: 21.1 MB (25.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,23 +1,41 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #Solution 1
+        #Solution 1 Arrap Preporcessing
+        # n = len(height)
+        # maxLeft = [0]*n
+        # maxRight = [0]*n
+        # maxL = 0
+        # maxR = 0
+        # for i in range(n):
+        #     j = n-1-i #for tracking the right side
+        #     maxLeft[i]=maxL
+        #     maxL = max(maxL,height[i])
+        #     maxRight[j]=maxR
+        #     maxR = max(maxR, height[j])
+        
+        # print(maxLeft)
+        # print(maxRight)
+        # totalWater = 0
+        # for i in range(1,n-1):
+        #     water = max(0, min(maxLeft[i],maxRight[i])-height[i])
+        #     totalWater += water
+        
+        # return totalWater
+
+        #Solution2 2 Pointers
         n = len(height)
-        maxLeft = [0]*n
-        maxRight = [0]*n
-        maxL = 0
-        maxR = 0
-        for i in range(n):
-            j = n-1-i #for tracking the right side
-            maxLeft[i]=maxL
-            maxL = max(maxL,height[i])
-            maxRight[j]=maxR
-            maxR = max(maxR, height[j])
-        
-        print(maxLeft)
-        print(maxRight)
+        l, r = 0, n-1
+        leftMax = 0
+        rightMax = 0
         totalWater = 0
-        for i in range(1,n-1):
-            water = max(0, min(maxLeft[i],maxRight[i])-height[i])
-            totalWater += water
-        
+        while l<r:
+            leftMax = max(leftMax, height[l])
+            rightMax = max(rightMax, height[r])
+            if leftMax<rightMax:
+                totalWater += max(0, leftMax-height[l])
+                l+=1
+            else:
+                totalWater +=  max(0, rightMax-height[r])
+                r-=1
+            
         return totalWater
