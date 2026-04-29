6ae6c9e71897777d8855093f1448fc97ddee679e
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-04-29 18:52:50 +0530

    Time: 20 ms (10.58%), Space: 21.4 MB (7.76%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,50 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        #Solution 1 - Array pre processing
-        # len_height = len(height)
-        # if len_height<3:
-        #     return 0
-        # left_max = [0]*len_height
-        # right_max = [0]*len_height
-
-        # for i in range(len_height):
-        #     if i==0:
-        #         left_max[i]=height[i]
-        #         right_max[len_height-1-i]=height[len_height-1-i]
-        #     else:
-        #         if left_max[i-1]<height[i]:
-        #             left_max[i]=height[i]
-        #         else:
-        #             left_max[i]=left_max[i-1]
-        #         if right_max[len_height-1-i+1]<height[len_height-1-i]:
-        #             right_max[len_height-1-i]=height[len_height-1-i]
-        #         else:
-        #             right_max[len_height-1-i]=right_max[len_height-1-i+1]
-
-        # result = 0
-        # for i in range(1,len_height-1):
-        #     water_at_i = min(left_max[i],right_max[i])-height[i]
-        #     water_at_i = max(0, water_at_i) #if somehow -ve water comes up
-        #     result = result+water_at_i
-
-        # return result
-
-        #Solution 2 - 2 Pointer Approach
-        if len(height)<3:
-            return 0
-        left = 0
-        right = len(height)-1
-        left_max = 0
-        right_max = 0
-        result = 0
-        while left<right:
-            left_max = max(left_max, height[left])
-            right_max = max(right_max, height[right])
-            if left_max<=right_max:
-                result += left_max-height[left]
-                left+=1
-            else:
-                result += right_max-height[right]
-                right-=1
-        return result
-
+        #Solution 1
+        n = len(height)
+        maxLeft = [0]*n
+        maxRight = [0]*n
+        maxL = 0
+        maxR = 0
+        for i in range(n):
+            j = n-1-i #for tracking the right side
+            maxLeft[i]=maxL
+            maxL = max(maxL,height[i])
+            maxRight[j]=maxR
+            maxR = max(maxR, height[j])
+        
+        print(maxLeft)
+        print(maxRight)
+        totalWater = 0
+        for i in range(1,n-1):
+            water = max(0, min(maxLeft[i],maxRight[i])-height[i])
+            totalWater += water
+        
+        return totalWater
