78350c69cf27cafed6d184c6eb3a6199b99f92ee
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-02 14:46:52 +0530

    Time: 11 ms (86.76%), Space: 19.2 MB (69.59%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -19,16 +19,26 @@ class Solution:
 
         # return goodNums
 
-        #Optimal using DP to use the precomputed results
-        hmap = {0:0, 1:1, 2:5, 5:2, 6:9, 9:6, 8:8}
-        goodNums=0
-        for num in range(1,n+1):
-            snum = str(num)
-            if '3' in snum or '4' in snum or '7' in snum:
+        #Optimal using memorization to use the precomputed results
+        # hmap = {0:0, 1:1, 2:5, 5:2, 6:9, 9:6, 8:8}
+        # goodNums=0
+        # for num in range(1,n+1):
+        #     snum = str(num)
+        #     if '3' in snum or '4' in snum or '7' in snum:
+        #         continue
+        #     else:
+        #         temp = hmap[num//10]*10+hmap[num%10]
+        #         hmap[num]=temp
+        #         if temp!=num:
+        #             goodNums+=1
+        # return goodNums
+
+        #Solution 3
+        res = 0
+        for num in range(1, n + 1):
+            curr = str(num)
+            if "3" in curr or "4" in curr or "7" in curr:
                 continue
-            else:
-                temp = hmap[num//10]*10+hmap[num%10]
-                hmap[num]=temp
-                if temp!=num:
-                    goodNums+=1
-        return goodNums
+            if "2" in curr or "5" in curr or "9" in curr or "6" in curr:
+                res += 1
+        return res
