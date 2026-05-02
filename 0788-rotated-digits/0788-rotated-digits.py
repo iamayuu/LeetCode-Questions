4ebdf21f4f0532c5dd0d4071de1b4d24468033d6
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-02 14:42:23 +0530

    Time: 15 ms (78.89%), Space: 19.5 MB (7.33%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,20 +1,34 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        #Brute Force
-        hmap = {'0':'0', '1':'1', '2':'5', '5':'2', '6':'9', '9':'6', '8':'8'}
-        def checkGood(num):
-            temp = ""
+        #Brute Force - Working under given constraints
+        # hmap = {'0':'0', '1':'1', '2':'5', '5':'2', '6':'9', '9':'6', '8':'8'}
+        # def checkGood(num):
+        #     temp = ""
+        #     snum = str(num)
+        #     if '3' in snum or '4' in snum or '7' in snum:
+        #         return False
+
+        #     for digit in snum:
+        #         temp+=hmap[digit]
+
+        #     return temp!=snum
+
+        # goodNums = 0
+        # for i in range(1,n+1):
+        #     goodNums+= 1 if checkGood(i) else 0
+
+        # return goodNums
+
+        #Optimal using DP to use the precomputed results
+        hmap = {0:0, 1:1, 2:5, 5:2, 6:9, 9:6, 8:8}
+        goodNums=0
+        for num in range(1,n+1):
             snum = str(num)
             if '3' in snum or '4' in snum or '7' in snum:
-                return False
-
-            for digit in snum:
-                temp+=hmap[digit]
-
-            return temp!=snum
-
-        goodNums = 0
-        for i in range(1,n+1):
-            goodNums+= 1 if checkGood(i) else 0
-
+                continue
+            else:
+                temp = hmap[num//10]*10+hmap[num%10]
+                hmap[num]=temp
+                if temp!=num:
+                    goodNums+=1
         return goodNums
