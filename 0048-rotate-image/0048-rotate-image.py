bb9425d2b894fc91fdc1c4a29fc8c359781c8ba6
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-04 10:28:44 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.02%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,15 +3,27 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        hmap = dict()
-        n = len(matrix)
-        for i in range(n):
-            for j in range(n):
-                hmap[(i,j)]=matrix[i][j]
+        #Soln1 using hmap to store old values
+        # hmap = dict()
+        # n = len(matrix)
+        # for i in range(n):
+        #     for j in range(n):
+        #         hmap[(i,j)]=matrix[i][j]
         
-        for key,value in hmap.items():
-            old_i=key[0]
-            old_j=key[1]
-            new_i = old_j
-            new_j = (n-1-old_i)
-            matrix[new_i][new_j]=value
+        # for key,value in hmap.items():
+        #     old_i=key[0]
+        #     old_j=key[1]
+        #     new_i = old_j
+        #     new_j = (n-1-old_i)
+        #     matrix[new_i][new_j]=value
+
+        #Solution2,optimal 
+        n = len(matrix)
+        
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        
+        for row in matrix:
+            row.reverse()
