2590fe012f3496f9d54e806ca7ee254af734aae0
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-09 12:36:03 +0530

    Time: 3 ms (1.28%), Space: 19.4 MB (18.22%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,3 +1,9 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return a+b
+        mask = 0xffffffff
+
+        while (mask&b)>0:
+            sum = a^b
+            carry = a&b
+            a,b = sum, carry<<1
+        return (mask&a) if b>0 else a
