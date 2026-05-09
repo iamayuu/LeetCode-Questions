223a7729b9adf1f65338249e8d9f1a6c37322213
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-09 20:46:41 +0530

    Time: 7 ms (72.36%), Space: 22.5 MB (49.72%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -4,12 +4,19 @@ class Solution:
         #take gray code of n-1, reverse the array and store it seperately lets say new_arr
         #take new_arr and add 2^(n-1) to each number
         #finally concat the two array
-        if n==1:
-            return [0,1]
+        # if n==1:
+        #     return [0,1]
         
-        arr = self.grayCode(n-1)
-        rev_arr = arr[::-1]
-        m = len(rev_arr)
-        for i in range(m):
-            rev_arr[i]+=2**(n-1)
-        return arr+rev_arr
+        # arr = self.grayCode(n-1)
+        # rev_arr = arr[::-1]
+        # m = len(rev_arr)
+        # for i in range(m):
+        #     rev_arr[i]+=2**(n-1)
+        # return arr+rev_arr 
+
+        #Solution 2
+        #THE Math trick - gray_code(i)=i xor (i>>1)
+        ans = []
+        for i in range(2**n):
+            ans.append(i^(i>>1))
+        return ans
