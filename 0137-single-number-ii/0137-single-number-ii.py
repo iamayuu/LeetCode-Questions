3c76c998a2306d43504860aeae6695b752cb5ffc
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-10 14:50:06 +0530

    Time: 4 ms (46.02%), Space: 20.7 MB (46.3%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -3,11 +3,8 @@ class Solution:
         if len(nums)==1:
             return nums[0]
         nums.sort()
-        if nums[0]!=nums[1]:
-            return nums[0]
-        elif nums[-1]!=nums[-2]:
-            return nums[-1]
         
         for i in range(1,len(nums),3):
             if nums[i]!=nums[i-1]:
                 return nums[i-1]
+        return nums[-1]
