6def6429c18ddfd3b6d1dd0c3a99b126c08a3c6f
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-12 10:31:06 +0530

    Time: 59 ms (43.56%), Space: 55.7 MB (30.69%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,24 +1,43 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        def isPossible(mid, tasks):
-            for task in tasks:
-                if mid>=task[1]:
-                    mid = mid-task[0]
-                else:
-                    return False
-            return True
+        #Solution 1 Binary Search + Custom Sorting
+        # def isPossible(mid, tasks):
+        #     for task in tasks:
+        #         if mid>=task[1]:
+        #             mid = mid-task[0]
+        #         else:
+        #             return False
+        #     return True
 
-        tasks.sort(key = lambda x:x[1]-x[0], reverse = True)
+        # tasks.sort(key = lambda x:x[1]-x[0], reverse = True)
 
-        l = 0
-        r = 10**9
-        ans = float("inf")
-        while l<=r:
-            mid = (r+l)//2
-            if isPossible(mid, tasks):
-                ans = min(ans,mid)
-                r = mid-1
-            else:
-                l=mid+1
+        # l = 0
+        # r = 10**9
+        # ans = float("inf")
+        # while l<=r:
+        #     mid = (r+l)//2
+        #     if isPossible(mid, tasks):
+        #         ans = min(ans,mid)
+        #         r = mid-1
+        #     else:
+        #         l=mid+1
         
-        return ans
+        # return ans
+
+        #Solutuion 2 Greedy
+        tasks.sort(key = lambda x:x[1]-x[0], reverse = True)
+        currEnergy = 0
+        minTotal = 0
+        for task in tasks:
+            actual = task[0]
+            minimum = task[1] 
+            if currEnergy<minimum:
+                extra  = minimum-currEnergy
+                minTotal+=extra
+                currEnergy=currEnergy+extra
+                currEnergy=currEnergy-actual
+            else:
+                currEnergy=currEnergy-actual
+
+        return minTotal
+
