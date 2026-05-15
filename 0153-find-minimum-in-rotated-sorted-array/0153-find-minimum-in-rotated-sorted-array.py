962d6f0ed9c945fa0eac42c9491be244d1cfca3b
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-15 11:23:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (66.55%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,18 +1,31 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        if len(nums)<=2:
-            return min(nums)
-        l = 0 
-        r = len(nums)-1
-        while l<=r:
-            m = (l+r)//2
-            if nums[m-1]>nums[m] and nums[m+1]>nums[m]:
-                return nums[m]
-            elif nums[m+1]<nums[m]:
-                return nums[m+1]
+        # if len(nums)<=2:
+        #     return min(nums)
+        # l = 0 
+        # r = len(nums)-1
+        # while l<=r:
+        #     m = (l+r)//2
+        #     if nums[m-1]>nums[m] and nums[m+1]>nums[m]:
+        #         return nums[m]
+        #     elif nums[m+1]<nums[m]:
+        #         return nums[m+1]
+        #     else:
+        #         if nums[r]<nums[m]:
+        #             l=m+1
+        #         else:
+        #             r=m-1
+        # return l
+
+        #Better Solution
+
+        n = len(nums)
+        l,r = 0, n-1
+
+        while l<r:
+            mid = (l+r)//2
+            if nums[r]<nums[mid]:
+                l=mid+1
             else:
-                if nums[r]<nums[m]:
-                    l=m+1
-                else:
-                    r=m-1
-        return l
+                r=mid
+        return nums[l]
