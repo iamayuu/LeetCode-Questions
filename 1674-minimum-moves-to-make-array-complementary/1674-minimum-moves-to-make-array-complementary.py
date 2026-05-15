6b39ccb4531461ced6d0c3a8468ae10a9a565ebc
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-16 01:49:16 +0530

    Time: 265 ms (14.21%), Space: 33.9 MB (7.92%) - LeetHub

--- 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.py
@@ -1,17 +1,57 @@
 class Solution:
-    def minMoves(self, nums: list[int], limit: int) -> int:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        #Brute Force - T.L.E.
+        # n = len(nums)
+        # minTarget = 2
+        # maxTarget = 2*limit
+        # ans = float("inf") #minimum moves
+        
+        # def movesReq(target,a,b):
+        #     if a+b==target:
+        #         return 0
+        #     if target>=(min(a,b)+1) and target<=(max(a,b)+limit):
+        #         return 1
+        #     return 2
+
+        # for target in range(minTarget, maxTarget+1):
+        #     moves = 0
+        #     for i in range(n//2):
+        #         a = nums[i]
+        #         b = nums[n-1-i]
+        #         moves+= movesReq(target,a,b)
+
+        #     ans=min(ans,moves)
+
+        # return ans
+
+        #Optimization using D.A.T.
+        def movesReq(target,a,b):
+            if a+b==target:
+                return 0
+            if target>=(min(a,b)+1) and target<=(max(a,b)+limit):
+                return 1
+            return 2
         n = len(nums)
-        diff = [0] * (2 * limit + 2)
-        for i in range(n // 2):
-            vala, valb = sorted([nums[i], nums[n - 1 - i]])
-            diff[2] += 2
-            diff[2 * limit + 1] -= 2
-            diff[vala + 1] -= 1
-            diff[valb + limit + 1] += 1
-            diff[vala + valb] -= 1
-            diff[vala + valb + 1] += 1
-        res, cur = n, 0
-        for i in range(2, 2 * limit + 1):
-            cur += diff[i]
-            res = min(res, cur)
-        return res
+        arr = [0]*((2*limit)-1)
+        m = len(arr)
+        arr[0]=n
+        for i in range(n//2):
+            a = nums[i]
+            b = nums[n-1-i]
+            minOf1Move = min(a,b)+1
+            maxOf1Move = max(a,b)+limit
+            arr[minOf1Move-2]-=1
+            if (maxOf1Move-1)<m:
+                arr[maxOf1Move-1]+=1
+            arr[a+b-2]-=1
+            if (a+b-1)<m:
+                arr[a+b-1]+=1
+        
+        ans = arr[0]
+        
+        for i in range(1,m):
+            arr[i]=arr[i-1]+arr[i]
+            ans = min(ans,arr[i])
+
+        return ans
+
