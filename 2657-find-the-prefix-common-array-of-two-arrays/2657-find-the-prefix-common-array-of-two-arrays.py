83e19ea8ffa739fc9b479d0586270ffa09352de4
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-05-20 12:43:41 +0530

    Time: 29 ms (21.45%), Space: 19.1 MB (98.58%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -14,21 +14,43 @@ class Solution:
         # return arr
 
         #Solution 2
-        seen = set()
-        n = len(A)
-        arr = [0]*n
-        for i in range(n):
-            if A[i] in seen:
-                arr[i]+=1
-            if B[i] in seen:
-                arr[i]+=1
-            if A[i]==B[i]:
-                arr[i]+=1
+        # seen = set()
+        # n = len(A)
+        # arr = [0]*n
+        # for i in range(n):
+        #     if A[i] in seen:
+        #         arr[i]+=1
+        #     if B[i] in seen:
+        #         arr[i]+=1
+        #     if A[i]==B[i]:
+        #         arr[i]+=1
 
-            seen.add(A[i])
-            seen.add(B[i])
+        #     seen.add(A[i])
+        #     seen.add(B[i])
         
-        for i in range(1,n):
-            arr[i]+=arr[i-1]
+        # for i in range(1,n):
+        #     arr[i]+=arr[i-1]
+        
+        # return arr
+
+        #Solution 3
+        n = len(A)
+        maskA , maskB = 0, 0
+        arr = []
+        def totalSetBits(n):
+            count=0
+            while n!=0:
+                n = n&(n-1)
+                count+=1
+
+            return count
+
+        for i in range(n):
+            maskA = maskA | (1<<A[i])
+            maskB = maskB | (1<<B[i])
+
+            final = maskA & maskB
+
+            arr.append(totalSetBits(final))
         
         return arr
