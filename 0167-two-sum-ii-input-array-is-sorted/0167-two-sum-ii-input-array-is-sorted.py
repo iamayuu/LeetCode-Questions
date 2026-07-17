214d65da3102bb245e0ba842a083144ae11b8e8c
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-07-17 09:45:41 +0530

    Time: 4 ms (50.11%), Space: 20.5 MB (75.37%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -10,13 +10,25 @@ class Solution:
         # return [0,1]
 
         #Optimal
-        hmap = dict()
-        n = len(numbers)
-        for i in range(n):
-            num = numbers[i]
-            if target-num in hmap:
-                return [hmap[target-num]+1, i+1]
-            if num not in hmap:
-                hmap[num]=i
+        # hmap = dict()
+        # n = len(numbers)
+        # for i in range(n):
+        #     num = numbers[i]
+        #     if target-num in hmap:
+        #         return [hmap[target-num]+1, i+1]
+        #     if num not in hmap:
+        #         hmap[num]=i
         
+        # return [0,1]
+
+        # 2 pointers
+        l, r = 0, len(numbers)-1
+
+        while l<r:
+            if numbers[l]+numbers[r]==target:
+                return [l+1,r+1]
+            elif numbers[l]+numbers[r]>target:
+                r-=1
+            else:
+                l+=1
         return [0,1]
