efc2521905144cca83086547f4c73187d23944ee
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-07-18 17:08:41 +0530

    Time: 27 ms (83.42%), Space: 31.4 MB (73.97%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,20 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #Solution1 (Brute Force) [Time Complexity - 0(n^2)]
-        # max_sum = float("-inf")
-        # for i in range(len(nums)):
-        #     sum=0
-        #     for j in range(i, len(nums)):
-        #         sum+=nums[j]
-        #         max_sum = max(sum,max_sum)
-        # return max_sum
-
-        #Solution2 (Kadane's Algorithm) [Time Complexity - 0(n)]
-        sum = 0
         max_sum = float("-inf")
-        for n in nums:
-            sum = sum+n
-            max_sum = max(sum,max_sum)
-            if sum<0:
-                sum=0
+        crr_sum = 0
+        for num in nums:
+            crr_sum += num
+            max_sum = max(crr_sum,max_sum)
+            if crr_sum < 1:
+                crr_sum=0
         return max_sum
