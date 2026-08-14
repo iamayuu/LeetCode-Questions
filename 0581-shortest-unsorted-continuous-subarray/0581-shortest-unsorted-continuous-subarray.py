e24225bee6f293ef68cb7a45a6f0af3ad969b99b
Ayush Raghuwanshi <ayushraghuwanshi54321@gmail.com>
2026-08-14 07:49:18 +0530

    Time: 15 ms (39.28%), Space: 20.4 MB (98.24%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.py
@@ -3,15 +3,12 @@ class Solution:
         n = len(nums)
         sorted_nums = sorted(nums)
         l, r = 0, n-1
-        while l<n:
+        while l<r:
             if nums[l]==sorted_nums[l]:
                 l+=1
-            else:
-                break
-        while r>-1:
-            if nums[r]==sorted_nums[r]:
+            elif nums[r]==sorted_nums[r]:
                 r-=1
             else:
                 break
         
-        return 0 if r<l else (r-l+1)
+        return 0 if r==l else (r-l+1)
